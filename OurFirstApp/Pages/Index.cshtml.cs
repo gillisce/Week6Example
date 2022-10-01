587c9fec1a6bce29b5b309c4e63f89a53d882e77
Chris Gillis <cgillis@starkstate.edu>
2022-10-01 14:27:38 -0400

    Add project files.

---
+++ OurFirstApp/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OurFirstApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string? Message { get; set; }
+        
+
+
+        public void OnGet()
+        {
+            Message = $"Hello World {DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}

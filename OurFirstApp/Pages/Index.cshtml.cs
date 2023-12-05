876b5a0db13ba5c84c69e12b3b650253cdb29d7a
Chris Gillis <cgillis93@gmail.com>
2023-12-05 16:31:54 -0500

    Add example of a ViewModel concept, and how to print out to the FE

--- OurFirstApp/Pages/Index.cshtml.cs
+++ OurFirstApp/Pages/Index.cshtml.cs
@@ -7,6 +7,12 @@ namespace OurFirstApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        //So now these are part of our IndexModel (View Model)
+            //We can go to the front end and access them
+            //you can add as many properties as you want here and be able to access them on the View
+        public List<Employee> EmployeeList {get;set;}
+        public List<Department> Departments {get;set;}
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -19,6 +25,8 @@ namespace OurFirstApp.Pages
         public void OnGet()
         {
             Message = $"Hello World {DateTime.Now.ToLongTimeString()}";
+
+            //Here we could use our Db Context, to call out and get the values to populate our EmployeeList and Departments just like we do on all the scaffolded pages for the individual database table
         }
     }
 }
